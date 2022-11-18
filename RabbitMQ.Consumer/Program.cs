b265070a8878b84248a39c2718625a96e6b941bf
Ahmad Abu Huzayma <ahuzayma@moj.gov.sa>
2022-11-18 19:38:36 +0300

    add new code

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+consumeExchangeMessage();
+
+void consumeExchangeMessage()
+{
+    ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = factory.CreateConnection())
+    using (var channal = connection.CreateModel())
+    {
+        var consumer = new EventingBasicConsumer(channal);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Message:{message} has been consumed by '{ea.ConsumerTag}'");
+            Console.WriteLine("==========================================================");
+        };
+        string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+        channal.BasicConsume(queue: "log.Info",
+            autoAck: true,// delete message after readed and send read acknowlgment
+            consumer: consumer,
+            consumerTag: "INFO");
+
+        channal.BasicConsume(queue: "log.Error",
+            autoAck: true,
+            consumer: consumer,
+            consumerTag: "Error");
+
+        channal.BasicConsume(queue: "log.all",
+            autoAck: true,
+            consumer: consumer,
+            consumerTag: "ALL");
+
+        Console.WriteLine("Press Enter To Exit");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQSample/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+
+//Run 
+example3();
+
+void example2()
+{
+    ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = factory.CreateConnection())
+    using (var channal = connection.CreateModel())
+    {
+        string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+        channal.BasicPublish(exchange: "",
+            routingKey: "",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine(" [x] Sent {0}",message);
+        Console.WriteLine("Press Enter To Exit");
+        Console.ReadLine();
+    }
+
+}
+
+void example3()
+{
+    ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = factory.CreateConnection())
+    using (var channal = connection.CreateModel())
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                string message = $"Log Info '{i}'";
+                var body = Encoding.UTF8.GetBytes(message);
+                channal.BasicPublish(exchange: "exTopic",
+                    routingKey: "log.Info",
+                    body: body);
+
+            }
+            else
+            {
+                string message = $"Log Error '{i}'";
+                var body = Encoding.UTF8.GetBytes(message);
+                channal.BasicPublish(exchange: "exTopic",
+                    routingKey: "log.Error",
+                    body: body);
+            }
+            Console.WriteLine(" [x] Sent {0}", i);
+            Thread.Sleep(2000);
+        }
+
+        Console.WriteLine("Press Enter To Exit");
+        Console.ReadLine();
+    }
+}
